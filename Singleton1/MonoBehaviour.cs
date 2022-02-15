79cb9a9babb5d4e06227f7238982035b320ffb11
sailymc <saily.mcabrerasanchez@gmail.com>
2022-02-15 16:40:08 -0400

    Add project files.

---
+++ Singleton1/MonoBehaviour.cs
@@ -0,0 +1,5 @@
+﻿// Singleton
+
+public class MonoBehaviour
+{
+}
---
+++ Singleton1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Singleton
+
+namespace Singleton;
+
+public class Ejemplo
+{
+    private static Ejemplo _instancia = null;
+    private Ejemplo()
+    {
+    }
+
+    public static Ejemplo Instancia
+    {
+        get
+        {
+            if (_instancia == null)
+                _instancia = new Ejemplo();
+            return _instancia;
+        }
+    }
+}
+
+
+
+
